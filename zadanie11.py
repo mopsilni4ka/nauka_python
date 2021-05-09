b32d575cd193728c9a8b8197ce92e038980845b7
ksusha <ksusha@users.noreply.github.com>
2021-05-09 12:39:48 +0000

    zadanie11

---
+++ zadanie11.py
@@ -0,0 +1,5 @@
+samochody = ['syrena', 'poloneza']
+ilosc = [3, 5]
+for idx in range( len(samochody) ):
+ print("idx: {0} {1}".format(idx, samochody[idx]))
+ print(samochody[idx] + " ma ilosc drzwi " + str(ilosc[idx]))
