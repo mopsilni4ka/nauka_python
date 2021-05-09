6381654accf6d4d8a2a30c6eb149db1def4d428c
ksusha <ksusha@users.noreply.github.com>
2021-05-09 11:55:43 +0000

    save zadanie8

---
+++ zadanie8.py
@@ -0,0 +1,9 @@
+samochody = ['syrena', 'poloneza', 'audi', 'bmw']
+
+print(len(samochody))
+
+for x in samochody:
+ print(x)
+
+for idx in range(len(samochody)) :
+ print("index: " + str(idx) + " : " + samochody[idx])
